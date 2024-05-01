0cf8686cff9148c62fa570121688930ca8dd9811
Neven Armanios <narmanios@gmail.com>
2024-05-01 19:07:21 -0400

    Update module_domestic_violence_reports.js

--- js/module_domestic_violence_reports.js
+++ js/module_domestic_violence_reports.js
@@ -1,7 +1,7 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 async function linechart_violence() {
-    const dataURL = "/data/domestic_violence_reports.json";
+    const dataURL = "./data/domestic_violence_reports.json";
 
     // Fetch the data from the JSON file
     const data = await d3.json(dataURL);
